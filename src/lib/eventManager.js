ec5793676da2ccc056c6ed3dd1fc2874470e4fb6
Jin Kwan Kim <kkole3897@gmail.com>
2025-04-02 03:05:03 +0900

    feat: eventManager 구현

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,5 +1,44 @@
-export function setupEventListeners(root) {}
+const eventListeners = new Map();
+const cleanupListeners = new Map();
 
-export function addEvent(element, eventType, handler) {}
+export function setupEventListeners(root) {
+  for (const { eventType, handler } of eventListeners.values()) {
+    root.addEventListener(eventType, handler);
+    cleanupListeners.set(handler, () => {
+      root.removeEventListener(eventType, handler);
+    });
+  }
+}
 
-export function removeEvent(element, eventType, handler) {}
+const generateEventListenerKey = (element, eventType, handler) => {
+  return JSON.stringify({ element, eventType, handler });
+};
+
+export function addEvent(element, eventType, handler) {
+  const createdHandler = (e) => {
+    if (e.target === element) {
+      handler(e);
+    }
+  };
+
+  const key = generateEventListenerKey(element, eventType, handler);
+
+  eventListeners.set(key, { eventType, handler: createdHandler });
+}
+
+export function removeEvent(element, eventType, handler) {
+  const key = generateEventListenerKey(element, eventType, handler);
+  const matchedEventListener = eventListeners.get(key);
+  let cleanup = null;
+  if (matchedEventListener) {
+    cleanup = cleanupListeners.get(matchedEventListener.handler);
+    eventListeners.delete(key);
+  }
+
+  console.log(cleanup);
+
+  if (cleanup) {
+    cleanup();
+    cleanupListeners.delete(matchedEventListener.handler);
+  }
+}
