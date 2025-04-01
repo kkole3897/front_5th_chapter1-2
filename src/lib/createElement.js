e22efd816f33ea96ad9b5537245d51ec596a7eb0
Jin Kwan Kim <kkole3897@gmail.com>
2025-04-02 02:19:34 +0900

    feat: createElement 구현

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,5 +1,63 @@
 import { addEvent } from "./eventManager";
 
-export function createElement(vNode) {}
+class UnsupportedElementTypeError extends Error {
+  static MESSAGE = "UnsupportedElementTypeError";
 
-function updateAttributes($el, props) {}
+  constructor() {
+    super(UnsupportedElementTypeError.MESSAGE);
+  }
+}
+
+export function createElement(vNode) {
+  if (
+    typeof vNode === "undefined" ||
+    typeof vNode === "boolean" ||
+    vNode === null
+  ) {
+    return document.createTextNode("");
+  }
+
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return document.createTextNode(String(vNode));
+  }
+
+  if (Array.isArray(vNode)) {
+    const fragment = document.createDocumentFragment();
+    vNode.forEach((child) => {
+      const $el = createElement(child);
+      fragment.appendChild($el);
+    });
+
+    return fragment;
+  }
+
+  if (typeof vNode.type === "function") {
+    throw new UnsupportedElementTypeError();
+  }
+
+  const { type, props, children } = vNode;
+  const $el = document.createElement(type);
+
+  if (props) {
+    updateAttributes($el, props);
+  }
+
+  children.forEach((child) => {
+    const $child = createElement(child);
+    $el.appendChild($child);
+  });
+
+  return $el;
+}
+
+function updateAttributes($el, props) {
+  Object.entries(props).forEach(([key, value]) => {
+    if (key.startsWith("on")) {
+      addEvent($el, key.slice(2).toLowerCase(), value);
+    } else if (key === "className") {
+      $el.className = value;
+    } else {
+      $el.setAttribute(key, value);
+    }
+  });
+}
