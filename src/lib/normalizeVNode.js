8680da055f6acba1eda1ebac4f649cd67a921a18
Jin Kwan Kim <kkole3897@gmail.com>
2025-04-02 01:51:41 +0900

    feat: normalizeVNode 구현

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,27 @@
 export function normalizeVNode(vNode) {
-  return vNode;
+  if (vNode == null || typeof vNode === "boolean") {
+    return "";
+  }
+
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return String(vNode);
+  }
+
+  if (typeof vNode.type === "function") {
+    const { type, props, children } = vNode;
+
+    return normalizeVNode(type({ ...props, children }));
+  }
+
+  const { type, props, children } = vNode;
+
+  const normalizedChildren = children
+    .map(normalizeVNode)
+    .filter((child) => child != null && child !== false && child !== "");
+
+  return {
+    type,
+    props,
+    children: normalizedChildren,
+  };
 }
