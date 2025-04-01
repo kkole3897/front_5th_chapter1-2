f00779e38a2dd3e42248f7e0b866b25093478155
Jin Kwan Kim <kkole3897@gmail.com>
2025-04-02 01:45:11 +0900

    fix: flattenedChildren 필터링 조건 수정

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -2,7 +2,9 @@ import { flattenDeep } from "./array/flatten-deep";
 
 // TODO: jsdoc 추가
 export function createVNode(type, props, ...children) {
-  const flattenedChildren = flattenDeep(children).filter(Boolean);
+  const flattenedChildren = flattenDeep(children).filter(
+    (child) => child !== null && child !== undefined && child !== false,
+  );
 
   return { type, props, children: flattenedChildren };
 }
