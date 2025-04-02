de77ef0279cc665addb785a5b4ccf0d4742785c0
Jin Kwan Kim <kkole3897@gmail.com>
2025-04-03 01:21:07 +0900

    feat: updateElement 구현

--- src/lib/renderElement.js
+++ src/lib/renderElement.js
@@ -1,15 +1,23 @@
 import { setupEventListeners } from "./eventManager";
 import { createElement } from "./createElement";
 import { normalizeVNode } from "./normalizeVNode";
-// import { updateElement } from "./updateElement";
+import { updateElement } from "./updateElement";
+
+let oldNode;
 
 export function renderElement(vNode, container) {
   // 최초 렌더링시에는 createElement로 DOM을 생성하고
   // 이후에는 updateElement로 기존 DOM을 업데이트한다.
   // 렌더링이 완료되면 container에 이벤트를 등록한다.
   const normalizedVNode = normalizeVNode(vNode);
-  const element = createElement(normalizedVNode);
 
-  container.replaceChildren(element);
+  if (!oldNode) {
+    const element = createElement(normalizedVNode);
+    container.replaceChildren(element);
+    oldNode = normalizedVNode;
+  } else {
+    updateElement(container, normalizedVNode, oldNode);
+  }
+
   setupEventListeners(container);
 }
--- src/lib/updateElement.js
+++ src/lib/updateElement.js
@@ -1,6 +1,117 @@
 import { addEvent, removeEvent } from "./eventManager";
 import { createElement } from "./createElement.js";
 
-function updateAttributes(target, originNewProps, originOldProps) {}
+function updateAttributes(target, originNewProps, originOldProps) {
+  if (!originNewProps && !originOldProps) {
+    return;
+  }
 
-export function updateElement(parentElement, newNode, oldNode, index = 0) {}
+  if (!originNewProps && originOldProps) {
+    for (const key of Object.keys(originOldProps)) {
+      if (key.startsWith("on")) {
+        removeEvent(target, key, originOldProps[key]);
+      } else if (key === "className") {
+        target.removeAttribute("class");
+      } else {
+        target.removeAttribute(key);
+      }
+    }
+  }
+
+  if (originNewProps && !originOldProps) {
+    for (const key of Object.keys(originNewProps)) {
+      if (key.startsWith("on")) {
+        addEvent(target, key, originNewProps[key]);
+      } else if (key === "className") {
+        target.className = originNewProps[key];
+      } else {
+        target.setAttribute(key, originNewProps[key]);
+      }
+    }
+  }
+
+  for (const [key, value] of Object.entries(originNewProps)) {
+    if (Object.keys(originOldProps).includes(key)) {
+      if (originOldProps[key] !== value) {
+        if (key.startsWith("on")) {
+          removeEvent(target, key, originOldProps[key]);
+          addEvent(target, key, value);
+        } else if (key === "className") {
+          target.className = value;
+        } else {
+          target.setAttribute(key, value);
+        }
+      }
+    } else {
+      if (key.startsWith("on")) {
+        addEvent(target, key, value);
+      } else if (key === "className") {
+        target.className = value;
+      } else {
+        target.setAttribute(key, value);
+      }
+    }
+  }
+
+  for (const key of Object.keys(originOldProps)) {
+    if (!Object.keys(originNewProps).includes(key)) {
+      if (key.startsWith("on")) {
+        removeEvent(target, key, originOldProps[key]);
+      } else {
+        target.removeAttribute(key);
+      }
+    }
+  }
+}
+
+export function updateElement(parentElement, newNode, oldNode, index = 0) {
+  if (oldNode != null && newNode == null) {
+    parentElement.removeChild(parentElement.children[index]);
+    return;
+  }
+
+  if (oldNode == null && newNode != null) {
+    const element = createElement(newNode);
+    parentElement.appendChild(element);
+    return;
+  }
+
+  if (typeof newNode === "string" || typeof newNode === "number") {
+    if (newNode !== oldNode) {
+      parentElement.textContent = newNode;
+    }
+    return;
+  }
+
+  if (oldNode.type !== newNode.type) {
+    const element = createElement(newNode);
+    parentElement.replaceChild(element, parentElement.children[index]);
+    return;
+  }
+
+  const element = parentElement.children[index];
+  const { props: newProps, children: newChildren } = newNode;
+  const { props: oldProps, children: oldChildren } = oldNode;
+
+  updateAttributes(element, newProps, oldProps);
+
+  if (!Array.isArray(newChildren)) {
+    if (Array.isArray(oldChildren)) {
+      for (const child of oldChildren) {
+        updateElement(element, null, child);
+      }
+    }
+    return;
+  }
+
+  if (!Array.isArray(oldChildren)) {
+    for (const child of newChildren) {
+      updateElement(element, child, null);
+    }
+    return;
+  }
+
+  for (let i = 0; i < newChildren.length; i++) {
+    updateElement(element, newChildren[i], oldChildren[i], i);
+  }
+}
